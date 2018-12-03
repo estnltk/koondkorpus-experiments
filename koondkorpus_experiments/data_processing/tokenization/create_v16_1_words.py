df370ab71170ccab248736322b1792d2e66f366d
pault <pault@ut.ee>
2018-12-03 10:24:08 +0200

    create_v16_1_words.py script

---
+++ koondkorpus_experiments/data_processing/tokenization/create_v16_1_words.py
@@ -0,0 +1,17 @@
+# crate EstNLTK 1.6 words layer on koondkorpus_base collection in 'estonian_text_corpora' schema
+# in 'estonian-text-corpora' database
+
+from estnltk.storage import PostgresStorage
+from koondkorpus_experiments.data_processing.tokenization.plain_word_tagger import PlainWordTagger
+
+
+storage = PostgresStorage(pgpass_file='~/.pgpass',
+                          dbname='estonian-text-corpora',
+                          schema='estonian_text_corpora',
+                          role='estonian_text_corpora_create')
+
+collection = storage.get_collection('koondkorpus_base')
+
+tagger = PlainWordTagger(output_layer='v16_1_words')
+
+collection.create_layer_buffered(tagger=tagger, progressbar='unicode')
