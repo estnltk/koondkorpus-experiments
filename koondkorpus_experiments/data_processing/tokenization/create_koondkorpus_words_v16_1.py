ebc119761de38bd86028f6e9ee96df70e36d7a9d
pault <pault@ut.ee>
2018-12-13 16:05:40 +0200

    create_koondkorpus_words_v16_1

---
+++ koondkorpus_experiments/data_processing/tokenization/create_koondkorpus_words_v16_1.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+# see readme.md for the description
+
+from estnltk import Text, Layer, Span
+from estnltk.storage.postgres.iterators import find_examples
+from estnltk.storage import PostgresStorage
+
+
+storage = PostgresStorage(pgpass_file='~/.pgpass',
+                          dbname='estonian-text-corpora',
+                          schema='estonian_text_corpora',
+                          role='estonian_text_corpora_create')
+
+collection = storage.get_collection('koondkorpus_base')
+
+output_collection = storage.get_collection('koondkorpus_words_v16_1',
+                                           meta_fields={'normalized_form': 'str',
+                                                        'word': 'str'})
+
+output_collection.create('example sentence for every word in koondkorpus_base__v16_1_words__layer')
+
+
+iter_word_examples = find_examples(collection=collection,
+                                   layer='v16_1_words',
+                                   attributes=['text'],
+                                   output_layers=['v16_1_words'],
+                                   return_text=True,
+                                   return_count=False,
+                                   progressbar='unicode')
+
+with output_collection.buffered_insert() as insert:
+    for example, key, span_pos, text in iter_word_examples:
+        span = text['v16_1_words'][span_pos]
+
+        sentence = None
+        for sentence in text.sentences:
+            if sentence.start <= span.start and span.end <= sentence.end:
+                break
+        assert sentence is not None
+
+        t_new = Text(sentence.enclosing_text)
+
+        layer = Layer(name='target', attributes=['normalized_form'], text_object=t_new)
+        span_new = Span(span.start-sentence.start, span.end-sentence.start)
+        span_new.normalized_form = span.normalized_form
+
+        layer.add_span(span=span_new)
+
+        t_new['target'] = layer
+
+        insert(t_new, meta_data={'word': span.text, 'normalized_form': span.normalized_form})
--- koondkorpus_experiments/data_processing/tokenization/readme.md
+++ koondkorpus_experiments/data_processing/tokenization/readme.md
@@ -1,7 +1,8 @@
 # Tokenization
 
 Various pipelines for re-tokenizing koondkorpus.
-## Layers
+## Collection `koondkorpus_base`
+### Layers
 * **v16_1_words:**
   Words obtained by EstNLTK 1.6 NLP pipeline (commit 47505126d7b7a4339113adfdc64576a10c7b1411)
 
@@ -9,6 +10,14 @@ Various pipelines for re-tokenizing koondkorpus.
 
   **Tagger:** [`PlainWordTagger`](plain_word_tagger.py) tags EstNLTK `Text` object with words layer ignoring all existing layers and using 
 only raw text as an input.
+
+## Collection `koondkorpus_words_v16_1`
+**Script:** [create_koondkorpus_words_v16_1.py](create_koondkorpus_words_v16_1.py) creates this collection.
+
+For every word in `v16_1_words` layer of `koondkorpus_base` collection there is an element in `koondkorpus_words_v16_1`
+collection. This element is a sentence that contains the word with `target` layer that marks the position of the word in
+the sentence and has the `normalized_form` attribute. The word text string and the normalized form is also saved as
+the collection metadata in the `word` and `normalized_form` columns.
 
 ## Notebooks
 
